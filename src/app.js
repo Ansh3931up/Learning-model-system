3a79fce177b4446a746f8e803a00a3f5482e531e
Ansh3931up <thebeliever39@gmail.com>
2024-07-26 12:44:07 +0530

    final porject

--- src/app.js
+++ src/app.js
@@ -8,7 +8,8 @@ import ApiResponse from "../utilities/ApiResponse.js";
 import morgan from "morgan";
 import userRouter from "../routes/user.routes.js";
 import blogRouter from "../routes/blog.routes.js";
-import paymentRouter from "../routes/payment.routes.js";
+import paymentRouter from "../routes/paymentList.routes.js";
+import paymentsRouter from "../routes/payment.routes.js";
 import photoRouter from "../routes/gallery.routes.js";
 const app=express();   
 const allowedOrigin = 'https://rss-react-anshs-projects-8a0075b6.vercel.app';
@@ -17,7 +18,7 @@ app.set("trust proxy", true);
 const corsOptions = {
   origin: allowedOrigin,
   credentials: true, // This allows cookies and credentials to be sent in cross-origin requests
-  optionsSuccessStatus: 200 // Some legacy browsers choke on 204
+  // optionsSuccessStatus: 200 // Some legacy browsers choke on 204
 };
 
 app.use(cors(corsOptions));
@@ -46,6 +47,7 @@ app.use("/api/v1/user",userRouter);
 //     throw new ApiError(404,"page not found");
 // })
 app.use("/api/v1/payment",paymentRouter);
+app.use("/api/v1/payments",paymentsRouter);
 app.use("/api/v1/photo",photoRouter)
 
 
